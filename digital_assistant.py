ea5b038ab99c9c896741cdce51134f73c229f130
Radhey Ruparel <123ruparel@gmail.com>
2022-11-22 17:38:43 -0700

    Update digital_assistant.py

--- digital_assistant.py
+++ digital_assistant.py
@@ -1,7 +1,5 @@
 #
 ##Author: Radhey Ruparel
-##Description: This program a very simplistic version of a digital assistant. The interface will be typed text on the python console. 
-##Also, it will only be able to recognize to main types of commands. It will also remember and recall something that user asks it to.
 #
 
 #Required to import all the os functions and progam is able to locate the file
